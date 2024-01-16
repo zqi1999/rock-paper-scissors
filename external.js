96eb46af6ce41a4127b3c6ea904c0ebd1b81d290
Zhaojie Qi <zqi1999@gmail.com>
2024-01-16 11:33:14 -0600

    Display player selection and computer selection in log
    Using console.log() shows the player's selection and the computer's selection so it would be easier to debug whether the logic is correct or not.

--- external.js
+++ external.js
@@ -1,14 +1,21 @@
 console.log("Hello");
 
-let moves = ['rock', 'paper', 'scissors', 'rock'];
-let playerSelection = prompt("Choose a move: ", "rock");
+let moves = ['rock', 'paper', 'scissors'];
+let playerPromptSelection = prompt("Choose a move: ", "rock");
 
 function getComputerChoice() {
     let option = Math.floor(Math.random() * 3);
-    console.log(moves[option]);
-    return moves[option];
+    console.log(`Computer selects ${moves[option]}`);
+    return option;
 }
 
-function playRound(playerSelection, computerSelection = getComputerChoice()) {
-    
-  }
+function playRound(playerSelection = playerPromptSelection, computerSelection = getComputerChoice()) {
+    console.log(`Player: ${playerSelection}     Computer: ${moves[computerSelection]}`)
+    let playerIndex = moves.indexOf(playerSelection);
+    if (playerIndex == moves.length - 1 && computerSelection == 0) {
+        
+    }
+    if (computerSelection == moves.length - 1 && playerIndex == 0) {
+
+    }
+}
