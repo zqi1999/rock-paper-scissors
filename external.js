7acb993d589278ee3123a2d1105361510a00f2d0
Zhaojie Qi <zqi1999@gmail.com>
2024-01-16 11:42:23 -0600

    Completed logics for playRound()

--- external.js
+++ external.js
@@ -1,21 +1,29 @@
 console.log("Hello");
 
-let moves = ['rock', 'paper', 'scissors'];
-let playerPromptSelection = prompt("Choose a move: ", "rock");
+let moves = ["rock", "paper", "scissors"];
 
 function getComputerChoice() {
-    let option = Math.floor(Math.random() * 3);
-    console.log(`Computer selects ${moves[option]}`);
-    return option;
+  let option = Math.floor(Math.random() * 3);
+  console.log(`Computer selects ${moves[option]}`);
+  return option;
 }
 
-function playRound(playerSelection = playerPromptSelection, computerSelection = getComputerChoice()) {
-    console.log(`Player: ${playerSelection}     Computer: ${moves[computerSelection]}`)
-    let playerIndex = moves.indexOf(playerSelection);
-    if (playerIndex == moves.length - 1 && computerSelection == 0) {
-        
-    }
-    if (computerSelection == moves.length - 1 && playerIndex == 0) {
-
-    }
+function playRound(
+  playerSelection = prompt("Choose a move: ", "rock"),
+  computerSelection = getComputerChoice()
+) {
+  console.log(
+    `Player: ${playerSelection}     Computer: ${moves[computerSelection]}`
+  );
+  let playerIndex = moves.indexOf(playerSelection);
+  if (playerIndex == moves.length - 1 && computerSelection == 0) {
+    return `You Lose! ${moves[computerSelection]} beats  ${playerSelection}`;
+  } else if (computerSelection == moves.length - 1 && playerIndex == 0) {
+    return `You Won! ${playerSelection} beats ${moves[computerSelection]}`;
+  } else if (computerSelection == playerIndex) {
+    return playRound();
+  }
+  return playerIndex > computerSelection
+    ? `You Won! ${playerSelection} beats ${moves[computerSelection]}`
+    : `You Lose! ${moves[computerSelection]} beats  ${playerSelection}`;
 }
